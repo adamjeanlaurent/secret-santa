9affcd11dfb2dbea213b5c41f5f2e8527685d5ce
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2019-12-31 12:34:58 -0500

    restart

--- secret-santa/Program.cs
+++ secret-santa/Program.cs
@@ -7,32 +7,7 @@ namespace secretsanta
     {
         public static void Main(string[] args)
         {
-            Dictionary<int, string> IDToEmployee = new Dictionary<int, string>(); 
-            string input = "";
-            while(true)
-            {
-                Console.Write("Enter Employee Name: ");
-                input = Console.ReadLine();
 
-                if (input == "done")
-                    break;
-                 
-                IDToEmployee.Add(IDToEmployee.Count + 1, input);
-            }
-
-            SecretSantaPair[] EmployeePairs = new SecretSantaPair[] {};
-            HashSet<int> AlreadyPairedIDs = new HashSet<int>();
-            
-            while(AlreadyPairedIDs.Count != EmployeePairs.Length)
-            {
-               
-            }
-        }
-
-        public static int genRandNum(int lowerBound, int upperBound)
-        {
-            Random r = new Random();
-            return r.Next(lowerBound, upperBound + 1);
         }
     }
 }
